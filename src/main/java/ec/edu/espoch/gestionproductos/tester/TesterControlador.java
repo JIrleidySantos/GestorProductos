139f7e093a34005a7009e1d794ffcbba15035ebb
Dilankl30 <lucerodk7@gmail.com>
2025-02-13 13:51:23 -0500

    Controlador

---
+++ src/main/java/ec/edu/espoch/gestionproductos/tester/TesterControlador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.gestionproductos.tester;
+
+import ec.edu.espoch.gestionproductos.modelo.Producto;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class TesterControlador {
+    
+        
+    public void prueba(Producto objProducto){
+        
+        System.out.println("Titulo: "+ objProducto.getProducto());
+        System.out.println("Descripcion: "+objProducto.getPrecio());
+        System.out.println("Estado: "+objProducto.isEstado());
+        
+    }
+    
+}
